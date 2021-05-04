ded36ca478264cc86f7d22013a418a0606c3542d
안진흥 <wlsgmd1535@naver.com>
2021-05-04 23:54:37 +0900

    2021. 05. 04

---
+++ framework/class/day6/guestbook_springboot/src/main/java/com/ssafy/guestbook/webConfiguration.java
@@ -0,0 +1,22 @@
+package com.ssafy.guestbook;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.ssafy.guestbook.interceptor.ConfirmInterceptor;
+
+@Configuration
+public class webConfiguration extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private ConfirmInterceptor confirmInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(confirmInterceptor).addPathPatterns("/article/**");
+	}
+	
+}
